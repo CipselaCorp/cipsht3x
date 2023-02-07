ab8e31a3228626136b806edf391178f646bc0225
CipselaCorp <124727402+CipselaCorp@users.noreply.github.com>
2023-02-07 14:46:07 -0500

    Se añadió la temperatura

--- main.ts
+++ main.ts
@@ -26,8 +26,36 @@ namespace CIPSHT3X {
 
     /**
      * Read Temperature in degrees celcius from the SHT2x sensor.
-     * Returns a number des…
+     * Returns a number describing the ambient temperature in degrees celsius
     */
+    //% blockId="SHT3xDriver_read_temperature"
+    //% block="Leer Temperatura"
+    export function leer_temperatura(): number {
+        Reset;
+        setReg(SHT31_MEAS_HIGHREP);
+        //let buf = pins.createBuffer(2);
+        //pins.i2cWriteNumber(SHT31_DEFAULT_ADDR, SHT31_MEAS_HIGHREP, NumberFormat.UInt8BE, false);
+
+        basic.pause(100);
+        let i2cBuffer = pins.i2cReadBuffer(SHT31_DEFAULT_ADDR, pins.sizeOf(NumberFormat.UInt16LE) * 7, false);
+
+        let result = i2cBuffer[0] << 8;
+        result |= i2cBuffer[1];
+        result = ((4375 * result) >> 14) - 4500;
+        let temp = result / 100;
+        return temp
+    }
+    //serial.writeBuffer(i2cBuffer);
+    //serial.readBuffer(result);
+    //basic.showNumber(temp);
+    //basic.pause(1000)
+    //CRC8(i2cBuffer[0], i2cBuffer[1], i2cBuffer[2]);
+    //basic.showNumber(i2cBuffer[0]);
+    /**
+    * Read Relative Humidity from the SHT2x Sensor.
+    * Returns a number describing the relative humidity in percentage % relative
+    * humidity
+   */
     //% blockId="SHT3xDriver_read_humidity"
     //% block="Leer Humedad"
     export function leer_humedad(): number {
